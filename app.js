6c7ae6f92f65bc007656917a6193676b8aacee27
zairita48 <zairanieto29@gmail.com>
2025-04-19 23:14:37 -0500

    modifique otravez

---
+++ app.js
@@ -0,0 +1,10 @@
+console.log("¡Hola desde JavaScript en VS Code!")
+
+function suma(a, b) {
+  return a + b;   
+}
+
+console.log ("la suma de 2 + 3 es:", suma (2, 3));
+function mostrarmMensaje() {
+    alert('¡Bienvenido a JavaScript Interno!');             
+    }
