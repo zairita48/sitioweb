edc5870219eaed3ec42efca94ad1b092d8d2267a
zairita48 <zairanieto29@gmail.com>
2025-04-21 22:10:27 -0500

    modifique 2

---
+++ javascrip/app.js
@@ -0,0 +1,20 @@
+let x = 10; //tipo de dato entero
+let y = 3.14; //tipo de dato decimal
+let nombre = "zaira"; //tipo de dato cadena de texto
+let esActivo = true; //tipo de dato booleano
+
+if (x > y) { 
+  console.log("El número mayor es x:", x);
+} else {
+  console.log("El número mayor es y:", y);
+}
+
+x = 1;
+while (x <= 10) {
+  console.log("soy el numero", x)
+  x++;
+}
+
+for (y=1;y <=10; y++){ 
+  console.log("soy el numero",y);
+ }
